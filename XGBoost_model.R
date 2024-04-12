b68b8d790c5791cd0484a03abf630cf317b5bf57
Trungpham2k4 <120292027+Trungpham2k4@users.noreply.github.com>
2024-04-12 08:18:32 +0700

    Update file

--- XGBoost_model.R
+++ XGBoost_model.R
@@ -8,7 +8,8 @@ library(gbm)
 data1 <- read.table(file.choose(), header = T, sep = ",")
 
 set.seed(123)
-#Test bằng cách sample trên github của bài mẫu
+
+# First we will separate the data set to 2 parts, one for training and the other for testing
 parts <- sample(1:nrow(data1), nrow(data1) * 0.8)
 
 train <- data1[parts,]
@@ -27,37 +28,24 @@ xgb_test = xgb.DMatrix(data = test_x, label = test_y)
 #define watchlist
 watchlist = list(train=xgb_train, test=xgb_test)
 
-
 param_list <- list(booster="gbtree",eta = 0.1, max_depth = 6)
 
 #define final model
-
-#final = xgb.train(params = param_list, data = xgb_train, watchlist=watchlist, nrounds = 100)
 final = xgboost(params = param_list, data = xgb_train, nrounds = 200, verbose = 0)
-
-#final <- gbm(formula = tdp ~ bfreq + ncore + temp, data = train, 
-#             distribution = "gaussian", 
-#             n.trees = 1000, shrinkage = 0.1,
-#             interaction.depth = 6,
-#             bag.fraction = 1,
-#             n.minobsinnode = 1)
-
 print(final)
 
 #use model to make predictions on test data
 pred_tdp = predict(final, xgb_test)
 
-#pred_tdp = predict(final, newdata = test)
-
 accuracyTest <- as.data.frame(test_y, row.names = NULL)
 accuracyTest["Predicted_tdp"] <- as.data.frame(pred_tdp, row.names = NULL)
 
 #measure prediction accuracy
-mean((test_y - pred_tdp)^2) #mse
-sum((test_y - pred_tdp)^2) 
-MAE_ofGB <- caret::MAE(test_y, pred_tdp) #mae
-caret::RMSE(test_y, pred_tdp) #rmse
+print(paste("MSE: ",mean((test_y - pred_tdp)^2))) #mse
+print(paste("MAE: ",caret::MAE(test_y,pred_tdp))) #mae
+print(paste("RMSE: ",caret::RMSE(test_y,pred_tdp))) #rmse
 
+# Calculate R squared on testing đata
 r2_check <- cor(accuracyTest$test_y, accuracyTest$Predicted_tdp)^2
 print(r2_check)
 
@@ -70,8 +58,8 @@ qqline(residualsGB)
 
 # Plotting the predicted - actual
 ggplot(accuracyTest, aes(x = test_y, y = accuracyTest$Predicted_tdp)) + 
-  geom_point(shape=1, color="blue") +
-  geom_abline(mapping=aes(intercept= 0, slope=1),color="darkblue") + 
+  geom_point(shape=1, color="darkblue") +
+  geom_abline(mapping=aes(intercept= 0, slope=1),color="red") + 
   labs(x = "TDP", y = "TDP Predicted")
 
 
